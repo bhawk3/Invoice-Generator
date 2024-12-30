ec2dbcba03160e92860b3c93f714c2d6a2c92215
Brendan Hawk <brendanhawk03@aol.com>
2024-12-30 08:40:15 -0500

    UI completed & JS started
    The majority of the UI is completed besides button hover effects. Event listeners created on buttons.

---
+++ script.js
@@ -0,0 +1,22 @@
+/* There may be an issue with class and id in 
+the html for the line items/buttons/prices */
+
+
+//Car Wash function
+function carWash() {
+    console.log("car wash")
+} 
+
+//Car Detail function
+function carDetail() {
+
+    //innerHTML should work fine in this use case bc youre not getting user input
+    console.log("car detail")
+ 
+}
+
+//Car Shine function
+function carShine() {
+    console.log("car shine")
+
+}
