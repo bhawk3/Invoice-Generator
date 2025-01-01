db4f3edac300a92db11287a8b99c8f76167b4ea1
Brendan Hawk <brendanhawk03@aol.com>
2025-01-01 15:37:31 -0500

    Working on removing last Service Item of that kind when remove btn clicked

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ const services = []
 const totalAmountString = document.getElementById("total-amount")
 let totalAmount = 0
 
+const carWashHTML = `<div class="col-6"style="justify-content: center;"><p class="line-item-service">Car Wash</p><button type="button" class="line-item-service-btn" onclick="cwRemoveBtn()">Remove</button></div>
+        <div class="col-6" style="justify-content: center;" id="line-item-price">$ 10</div>`
+
+
 //Car Wash function
 function carWash() {
    
     const newCarWashServiceContent = document.createElement('div');
     newCarWashServiceContent.className = "d-flex"
-    newCarWashServiceContent.innerHTML = `<div class="col-6"style="justify-content: center;"><p class="line-item-service">Car Wash</p><button type="button" class="line-item-service-btn" onclick="cwRemoveBtn()">Remove</button></div>
-        <div class="col-6" style="justify-content: center;" id="line-item-price">$ 10</div>`
-
+    newCarWashServiceContent.innerHTML = carWashHTML
     serviceContainer.appendChild(newCarWashServiceContent)
     services.push(newCarWashServiceContent)
 
@@ -39,6 +41,7 @@ function carDetail() {
 //Car Shine function
 function carShine() {
 
+
     const newCarShineServiceContent = document.createElement('div')
     newCarShineServiceContent.className = "d-flex"
     newCarShineServiceContent.innerHTML = `<div class="col-6"style="justify-content: center;"><p class="line-item-service">Car Shine</p><button type="button" class="line-item-service-btn" onclick="csRemoveBtn()">Remove</button></div>
@@ -47,6 +50,8 @@ function carShine() {
     serviceContainer.appendChild(newCarShineServiceContent)
     services.push(newCarShineServiceContent)
 
+    console.log(services)
+
     let amount = totalAmount += 30
     totalAmountString.innerHTML = amount
 
@@ -54,11 +59,14 @@ function carShine() {
 
 //Car Wash Remove Btn
 function cwRemoveBtn() {
-   let cwRemove = services.pop()
+   let cwRemove = services.lastIndexOf(`<div class="col-6"style="justify-content: center;"><p class="line-item-service">Car Wash</p><button type="button" class="line-item-service-btn" onclick="cwRemoveBtn()">Remove</button></div>
+        <div class="col-6" style="justify-content: center;" id="line-item-price">$ 10</div>`)
+   cwRemoved = services.pop(cwRemove)
 
-   if (cwRemove) {
-        serviceContainer.removeChild(cwRemove)
-        totalAmount - 10
+   if (cwRemoved) {
+        serviceContainer.removeChild(cwRemoved)
+        
+        totalAmountString.innerHTML = totalAmount - 10
    }  
 }
 
@@ -68,7 +76,10 @@ function cdRemoveBtn() {
 
     if (cdRemove) {
         serviceContainer.removeChild(cdRemove)
-        totalAmount - 20
+       // totalAmount - 20
+
+        totalAmountString.innerHTML = totalAmount - 20
+
     }
 }
 
@@ -78,6 +89,8 @@ function csRemoveBtn() {
 
     if (csRemove) {
         serviceContainer.removeChild(csRemove)
-        totalAmount - 30
+      //  totalAmount - 30
+      totalAmountString.innerHTML = totalAmount - 30
+
     }
 }
